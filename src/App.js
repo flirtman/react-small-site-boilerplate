70d14079ca3686b9efec72971173716563305a34
flirtman <iliasvinin@gmail.com>
2018-11-10 14:26:22 -0500

    Footer for mobile version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+import {Dialog} from 'primereact/dialog';
+import {Accordion,AccordionTab} from 'primereact/accordion';
 
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
+
 import Navigation from "./components/Navigation";
+import FooterMobile from "./components/FooterMobile";
+
+
 
 class App extends Component {
   render() {
@@ -20,6 +30,7 @@ class App extends Component {
             <Route path="/contact" component={Contact} />
             <Route component={Error} />
           </Switch>
+          <FooterMobile/>
         </div>
       </BrowserRouter>
     );
