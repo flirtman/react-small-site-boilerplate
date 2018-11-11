293e0a0b0d7d75b433433045c6adc19407ab4271
flirtman <iliasvinin@gmail.com>
2018-11-10 22:48:24 -0500

    primeflex added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 
 import {Dialog} from 'primereact/dialog';
 import {Accordion,AccordionTab} from 'primereact/accordion';
+
+
+
 
 import Home from "./components/Home";
 import About from "./components/About";
